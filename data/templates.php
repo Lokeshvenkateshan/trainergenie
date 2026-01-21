51f53218e53a795e0fe35ff9c60e691a0df1b305
mohanrajloganathan05 <mohanrajloganathan2005@gmail.com>
2026-01-21 22:27:11 +0530

    added sample data for templates

---
+++ data/templates.php
@@ -0,0 +1,65 @@
+<?php
+$templates = [
+
+    [
+        "title" => "Card Games",
+        "desc" => "Create interactive deck-based learning scenarios.",
+        "image" => "./assets/images/card-game.jpeg",
+        "tag" => "Interactive"
+    ],
+    [
+        "title" => "Prioritization",
+        "desc" => "Help teams decide what matters most.",
+        "image" => "./assets/images/prioriti-pic.jpeg",
+        "tag" => "Strategy"
+    ],
+    [
+        "title" => "Growth Mindset",
+        "desc" => "Scenarios designed to foster resilience.",
+        "image" => "./assets/images/growth-mindset-pic.jpeg",
+        "tag" => "Soft Skills"
+    ],
+    [
+        "title" => "Prisoner’s Dilemma",
+        "desc" => "Classic game theory exercise.",
+        "image" => "./assets/images/prisioners-dilema-pic.webp",
+        "tag" => "Game Theory"
+    ],
+
+    [
+        "title" => "Team Collaboration",
+        "desc" => "Strengthen teamwork through problem-solving challenges.",
+        "image" => "./assets/images/team-collaboration.jpeg",
+        "tag" => "Leadership"
+    ],
+    [
+        "title" => "Decision Lab",
+        "desc" => "Practice structured and logical decision-making skills.",
+        "image" => "./assets/images/decision-pic.jpeg",
+        "tag" => "Strategy"
+    ],
+    [
+        "title" => "Conflict Resolver",
+        "desc" => "Learn effective techniques to manage workplace conflicts.",
+        "image" => "./assets/images/conflict-resolve-pic.jpeg",
+        "tag" => "Soft Skills"
+    ],
+    [
+        "title" => "Negotiation Skills",
+        "desc" => "Develop win-win negotiation strategies through scenarios.",
+        "image" => "./assets/images/negotiation-pic.jpeg",
+        "tag" => "Business"
+    ],
+    [
+        "title" => "Critical Thinking",
+        "desc" => "Improve analytical thinking with real-world situations.",
+        "image" => "./assets/images/critical-think-pic.jpeg",
+        "tag" => "Cognitive"
+    ],
+    [
+        "title" => "Ethical Challenges",
+        "desc" => "Explore ethical decision-making through guided exercises.",
+        "image" => "./assets/images/ethical-pic.png",
+        "tag" => "Ethics"
+    ]
+];
